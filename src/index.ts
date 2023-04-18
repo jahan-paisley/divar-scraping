9a375c3b81f7ee5ae812e3b823a8e521f9ec7ff7
Jahan Zinedine <jahan@paisley.digital>
2023-04-18 19:18:01 +0300

    minor changes including manual scraping trigger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 const {insertRecord, Listing, GeolocationCoord, buildSchema} = require('./buildSchema');
 
 console.clear();
-console.log("loaded");
 // buildSchema();
 
 let interval: number | null = null;
@@ -47,39 +46,50 @@ function equals(a: typeof Listing, b: typeof Listing): boolean {
 }
 
 const callback = function () {
-    console.log("loaded 1");
-    const contactBtn: HTMLElement | null =
-        document.querySelector("#app > div.kt-container > div button");
-
+    const contactBtn: HTMLElement | null = document.querySelector("#app > div.kt-container > div button");
+    let searchEl = document.querySelector('div.kt-nav-text-field__field > form > input') as HTMLInputElement;
+    if (searchEl!!.value!! != 'کردان')
+        return;
     document.querySelectorAll('article[class^=kt-post-card]').forEach(elem => {
-            if (
-                elem.textContent!!.indexOf('سهیلیه') != -1 ||
-                elem.textContent!!.indexOf('سهلیه') != -1 ||
-                elem.textContent!!.indexOf('سهیله') != -1 ||
-                elem.textContent!!.indexOf('چهارباغ') != -1 ||
-                elem.textContent!!.indexOf('چهار باغ') != -1 ||
-                elem.textContent!!.indexOf('هشتگرد') != -1 ||
-                elem.textContent!!.indexOf('زعفرانیه') != -1 ||
-                elem.textContent!!.indexOf('سرخاب') != -1 ||
-                elem.textContent!!.indexOf('افشاریه') != -1 ||
-                elem.textContent!!.indexOf('لشکرآباد') != -1 ||
-                elem.textContent!!.indexOf('لشکراباد') != -1 ||
-                elem.textContent!!.indexOf('لشگراباد') != -1 ||
-                elem.textContent!!.indexOf('لشگرآباد') != -1 ||
-                elem.textContent!!.indexOf('فشند') != -1 ||
-                elem.textContent!!.indexOf('اقدسیه') != -1 ||
-                elem.textContent!!.indexOf('طاووسیه') != -1 ||
-                elem.textContent!!.indexOf('کردان جنوب') != -1 ||
-                elem.textContent!!.indexOf('جنوب کردان') != -1 ||
-                elem.textContent!!.indexOf('کردان جنوبی') != -1 ||
-                elem.textContent!!.indexOf('رامجین') != -1 ||
-                elem.textContent!!.indexOf('تهران دشت') != -1 ||
-                elem.textContent!!.indexOf('تهراندشت') != -1
+            let content = elem.textContent;
+            if (content!!.indexOf('سهیلیه') != -1 ||
+                content!!.indexOf('سهلیه') != -1 ||
+                content!!.indexOf('سهیله') != -1 ||
+                content!!.indexOf('چهارباغ') != -1 ||
+                content!!.indexOf('چهار باغ') != -1 ||
+                content!!.indexOf('هشتگرد') != -1 ||
+                content!!.indexOf('زعفرانیه') != -1 ||
+                content!!.indexOf('سرخاب') != -1 ||
+                content!!.indexOf('افشاریه') != -1 ||
+                content!!.indexOf('لشکرآباد') != -1 ||
+                content!!.indexOf('لشکراباد') != -1 ||
+                content!!.indexOf('لشگراباد') != -1 ||
+                content!!.indexOf('لشگرآباد') != -1 ||
+                content!!.indexOf('فشند') != -1 ||
+                content!!.indexOf('اقدسیه') != -1 ||
+                content!!.indexOf('طاووسیه') != -1 ||
+                content!!.indexOf('کردان جنوب') != -1 ||
+                content!!.indexOf('جنوب کردان') != -1 ||
+                content!!.indexOf('کردان جنوبی') != -1 ||
+                content!!.indexOf('رامجین') != -1 ||
+                content!!.indexOf('تهران دشت') != -1 ||
+                content!!.indexOf('تهراندشت') != -1 ||
+                content!!.indexOf(' زکی آباد') != -1 ||
+                content!!.indexOf(' زکی‌آباد') != -1 ||
+                content!!.indexOf('تهران‌دشت') != -1 ||
+                content!!.indexOf('سعید آباد') != -1 ||
+                content!!.indexOf('سعیدآباد') != -1 ||
+                content!!.indexOf('تنکمان') != -1 ||
+                content!!.indexOf('سنقرآباد') != -1 ||
+                content!!.indexOf('سنقراباد') != -1 ||
+                content!!.indexOf('سنقر اباد') != -1 ||
+                content!!.indexOf('سنقر آباد') != -1
             ) elem.remove();
         }
     )
 
-    if (contactBtn == null) return;
+    if (contactBtn == null)
+        return;
     if (found && interval) {
         console.log("1- interval gonna removed", interval);
         clearInterval(interval);
@@ -91,13 +101,15 @@ const callback = function () {
         clearInterval(interval);
     }
     contactBtn!!.onclick = function () {
-        console.log("loaded 2");
         console.log("registering click callback");
+        const listing = extract_listing();
+
         setTimeout(async function () {
             const mobile: HTMLElement | null = document.querySelector("a[href^='tel:']");
-            console.log("mobile:", mobile);
-            if (!mobile) return;
+            if (!mobile)
+                return;
             const mobileAttr = mobile.attributes.getNamedItem('href');
+
             if (mobileAttr != null && interval != null) {
                 const mobileVal = mobileAttr.value.replace('tel:', '');
                 found = true;
@@ -106,104 +118,112 @@ const callback = function () {
                 console.log(document.querySelector("[class*='kt-page-title__title']")!!.textContent);
                 console.log("3- interval gonna removed", interval);
                 clearInterval(interval);
-                const listing = new Listing({});
-                listing.created = listing.created || new Date()
-
-                const subtitle = document.querySelector('.kt-page-title__subtitle')!!.textContent!!
-                listing.district = subtitle.substring(subtitle.indexOf('در') + 2, subtitle.length).trim()
-
-                const len = document.querySelectorAll('.kt-breadcrumbs__link').length;
-                const cat = document.querySelectorAll('.kt-breadcrumbs__link')[len - 2]!!
-                    .attributes
-                    .getNamedItem('href')!!
-                    .value;
-                listing.category = cat.substring(cat.lastIndexOf('/') + 1, cat.length);
-
                 listing.mobile = parseInt(mobileVal)
-                listing.title = document.querySelector('[class^=kt-page-title__title]')!!.textContent!!
-                listing.desc = document.querySelector('[class^=kt-description-row__text]')!!.textContent!!
-                const elems: NodeListOf<Element> = document.querySelectorAll('.kt-group-row-item--info-row, ' +
-                    '.kt-unexpandable-row');
-                let url = window.location.href;
-                listing.ext_id = url.substring(url.lastIndexOf('/') + 1, url.length)
-                elems.forEach(o => {
-                    if (o.textContent!!.indexOf('متراژ زمین') != -1)
-                        listing.land_area = parseInt(o.textContent!!
-                            .replace('متراژ زمین', '')
-                            .replaceAll('٬', '')
-                            .replace('متر', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('قیمت کل') != -1)
-                        listing.price = parseInt(o.textContent!!
-                            .replace('قیمت کل', '')
-                            .replaceAll('٬', '')
-                            .replace('تومان', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('قیمت هر متر') != -1)
-                        listing.price_meter = parseInt(o.textContent!!
-                            .replace('قیمت هر متر', '')
-                            .replaceAll('٬', '')
-                            .replace('تومان', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('متراژ') != -1)
-                        listing.area = parseInt(o.textContent!!
-                            .replace('متراژ', '')
-                            .replaceAll('٬', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('ساخت') != -1)
-                        listing.built_at = parseInt(o.textContent!!
-                            .replace('ساخت', '')
-                            .replaceAll('٬', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('اتاق') != -1)
-                        listing.rooms = parseInt(o.textContent!!
-                            .replace('اتاق', '')
-                            .replaceAll('٬', '')
-                            .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
-                        );
-                    else if (o.textContent!!.indexOf('آگهی‌دهنده') != -1 || o.textContent!!.indexOf('آژانس املاک') != -1)
-                        listing.advertiser = o.textContent!!
-                            .replace('آگهی‌دهنده', '')
-                            .replace('آژانس املاک', 'آژانس املاک ');
-                    else
-                        console.log("not saved" + o.textContent!!)
-                });
-                const locElem = document.querySelector('a[class^=map]');
-                if (locElem) {
-                    const href = locElem.attributes.getNamedItem('href')!!.value;
-                    const regex = /latitude=([0-9.]+)&longitude=([0-9.]+)(?:&radius=([0-9.]+))?/;
-                    const array = regex.exec(href)!!;
-                    listing.location = new GeolocationCoord(Number(array[1]), Number(array[2]));
-                    listing.accuracy = Number((array.length >= 3 && array[3]) || 0);
-
-                }
                 console.log(listing)
                 // insertRecord(listing);
                 if (JSON.stringify(listing).indexOf('کردان') !== -1) {
                     const result = await query_es(listing);
                     if (result.length === 0) {
+                        console.log('inserting into ES');
                         insert_es(listing);
+                        alert('inserted new record');
                     } else if (result.filter(o => equals(listing, o._source)).length == 0) {
                         listing.revision = true;
+                        console.log('inserting the next revision into ES')
                         insert_es(listing);
+                        alert('inserted revised record');
+                    } else {
+                        alert('did nothing');
                     }
-                    alert('done');
                 }
             }
         }, 2000);
     };
     console.log("clicking the contact button ...");
-    contactBtn!!.click();
+    //contactBtn!!.click();
 };
 
-function callOnLocationChange() {
-    console.log("loaded 3");
+function extract_listing() {
+    const listing = new Listing({});
+    listing.created = listing.created || new Date()
 
+    const subtitle = document.querySelector('.kt-page-title__subtitle')!!.textContent!!
+    listing.district = subtitle.substring(subtitle.indexOf('در') + 2, subtitle.length).trim()
+
+    const len = document.querySelectorAll('.kt-breadcrumbs__link').length;
+    const cat = document.querySelectorAll('.kt-breadcrumbs__link')[len - 2]!!
+        .attributes
+        .getNamedItem('href')!!
+        .value;
+    listing.category = cat.substring(cat.lastIndexOf('/') + 1, cat.length);
+
+    listing.title = document.querySelector('[class^=kt-page-title__title]')!!.textContent!!
+    listing.desc = document.querySelector('[class^=kt-description-row__text]')!!.textContent!!
+    const elems: NodeListOf<Element> = document.querySelectorAll('.kt-group-row-item--info-row, ' +
+        '.kt-unexpandable-row');
+    let url = window.location.href;
+    listing.ext_id = url.substring(url.lastIndexOf('/') + 1, url.length)
+    const locElem = document.querySelector('a[class^=map]');
+    if (locElem) {
+        const href = locElem.attributes.getNamedItem('href')!!.value;
+        const regex = /latitude=([0-9.]+)&longitude=([0-9.]+)(?:&radius=([0-9.]+))?/;
+        const array = regex.exec(href)!!;
+        listing.location = new GeolocationCoord(Number(array[1]), Number(array[2]));
+        listing.accuracy = Number((array.length >= 3 && array[3]) || 0);
+
+    }
+
+    elems.forEach(o => {
+        if (o.textContent!!.indexOf('متراژ زمین') != -1)
+            listing.land_area = parseInt(o.textContent!!
+                .replace('متراژ زمین', '')
+                .replaceAll('٬', '')
+                .replace('متر', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('قیمت کل') != -1)
+            listing.price = parseInt(o.textContent!!
+                .replace('قیمت کل', '')
+                .replaceAll('٬', '')
+                .replace('تومان', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('قیمت هر متر') != -1)
+            listing.price_meter = parseInt(o.textContent!!
+                .replace('قیمت هر متر', '')
+                .replaceAll('٬', '')
+                .replace('تومان', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('متراژ') != -1)
+            listing.area = parseInt(o.textContent!!
+                .replace('متراژ', '')
+                .replaceAll('٬', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('ساخت') != -1)
+            listing.built_at = parseInt(o.textContent!!
+                .replace('ساخت', '')
+                .replaceAll('٬', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('اتاق') != -1)
+            listing.rooms = parseInt(o.textContent!!
+                .replace('اتاق', '')
+                .replaceAll('٬', '')
+                .replace(new RegExp(/[۰-۹]/g), d => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d).toString())
+            );
+        else if (o.textContent!!.indexOf('آگهی‌دهنده') != -1 || o.textContent!!.indexOf('آژانس املاک') != -1)
+            listing.advertiser = o.textContent!!
+                .replace('آگهی‌دهنده', '')
+                .replace('آژانس املاک', 'آژانس املاک ');
+        else
+            console.log("not saved" + o.textContent!!)
+    });
+    return listing;
+}
+
+function callOnLocationChange() {
     let previousUrl = '';
     const observer = new MutationObserver(function () {
         if (location.href !== previousUrl) {
